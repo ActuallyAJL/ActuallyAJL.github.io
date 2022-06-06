e9e2932ec5769b0752f4f1a608260ca80ea3a935
ActuallyAJL <zero3260@hotmail.com>
2022-06-06 15:58:31 -0500

    testing github pages

--- src/MyMagic.js
+++ src/MyMagic.js
@@ -3,33 +3,33 @@ import { ApplicationViews } from "./components/ApplicationViews";
 import "./MyMagic.css";
 
 export const MyMagic = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    sessionStorage.getItem("mm_user") !== null
-  );
+  // const [isAuthenticated, setIsAuthenticated] = useState(
+  //   sessionStorage.getItem("mm_user") !== null
+  // );
 
-  const setAuthUser = (user) => {
-    sessionStorage.setItem("mm_user", JSON.stringify(user));
-    setIsAuthenticated(sessionStorage.getItem("mm_user") !== null);
-  };
+  // const setAuthUser = (user) => {
+  //   sessionStorage.setItem("mm_user", JSON.stringify(user));
+  //   setIsAuthenticated(sessionStorage.getItem("mm_user") !== null);
+  // };
 
-  const clearUser = () => {
-    sessionStorage.clear();
-    setIsAuthenticated(sessionStorage.getItem("mm_user") !== null);
-  };
+  // const clearUser = () => {
+  //   sessionStorage.clear();
+  //   setIsAuthenticated(sessionStorage.getItem("mm_user") !== null);
+  // };
 
-  const getLoggedInUser = () => {
-    const thisUserId = parseInt(sessionStorage.getItem("mm_user"));
-    return thisUserId;
-  };
+  // const getLoggedInUser = () => {
+  //   const thisUserId = parseInt(sessionStorage.getItem("mm_user"));
+  //   return thisUserId;
+  // };
 
   return (
     <>
       <ApplicationViews
-        setAuthUser={setAuthUser}
-        clearUser={clearUser}
-        isAuthenticated={isAuthenticated}
-        setIsAuthenticated={setIsAuthenticated}
-        getLoggedInUser={getLoggedInUser}
+      // setAuthUser={setAuthUser}
+      // clearUser={clearUser}
+      // isAuthenticated={isAuthenticated}
+      // setIsAuthenticated={setIsAuthenticated}
+      // getLoggedInUser={getLoggedInUser}
       />
     </>
   );
--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,34 +1,36 @@
 import React from "react";
 import { Outlet, Navigate, Routes, Route } from "react-router-dom";
-import { Login } from "./auth/Login";
-import { Register } from "./auth/Register";
+// import { Login } from "./auth/Login";
+// import { Register } from "./auth/Register";
 import { CardList } from "./cards/CardList";
 import { CardDetails } from "./cards/CardDetails";
 import "../App.css";
 
-export const ApplicationViews = ({
-  isAuthenticated,
-  setAuthUser,
-  getLoggedInUser,
-  clearUser,
-}) => {
-  const PrivateOutlet = () => {
-    return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-  };
+export const ApplicationViews = (
+  {
+    // isAuthenticated,
+    // setAuthUser,
+    // getLoggedInUser,
+    // clearUser,
+  }
+) => {
+  // const PrivateOutlet = () => {
+  //   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+  // };
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<PrivateOutlet />}>
-          <Route path="/" element={<CardList />}></Route>
-          <Route path="/:cardId/details" element={<CardDetails />}></Route>
-        </Route>
+        {/* <Route path="/" element={<PrivateOutlet />}> */}
+        <Route path="/" element={<CardList />}></Route>
+        <Route path="/:cardId/details" element={<CardDetails />}></Route>
+        {/* </Route> */}
 
-        <Route path="/login" element={<Login setAuthUser={setAuthUser} />} />
+        {/* <Route path="/login" element={<Login setAuthUser={setAuthUser} />} />
         <Route
           path="/register"
           element={<Register setAuthUser={setAuthUser} />}
-        />
+        /> */}
       </Routes>
     </>
   );
